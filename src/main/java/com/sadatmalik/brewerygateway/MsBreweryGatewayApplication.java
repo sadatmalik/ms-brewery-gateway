796efb6f3184c6e1205e64848269cc39cd4b62f5
sadat.malik <sadat.malik@me.com>
2022-02-11 22:42:52 +0000

    Initial project files and gateway configuration

---
+++ src/main/java/com/sadatmalik/brewerygateway/MsBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.brewerygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBreweryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBreweryGatewayApplication.class, args);
+	}
+
+}
